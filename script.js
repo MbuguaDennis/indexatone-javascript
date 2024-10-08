f0d2660046dd9b2c8dc809bc28182fd3c5b157fb
DENNIS MBUGUA <dmbugua.developer@gmail.com>
2024-10-08 12:59:04 +0300

    test files update

---
+++ script.js
@@ -0,0 +1,31 @@
+// Javascript inline comment
+/*
+js multiline commments
+*/
+// variables in js
+/*
+BUNNS
+numbers
+string
+boolean
+bgint
+undefined
+null
+objects
+symbol
+*/
+let alwaysHappy= true;
+let currentStatus;
+let age = 26;
+let balanceAmount = null;
+let professional = 'Software engineer';
+console.log(typeof(alwaysHappy))
+console.log(typeof(currentStatus))
+console.log(typeof(age))
+console.log(typeof(balanceAmount))
+console.log(typeof(professional))
+
+
+
+
+
